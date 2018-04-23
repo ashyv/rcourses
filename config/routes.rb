dd983e20e72cf2afbd15bfff35f99fd9a7dd6f49
Ashwin <ashyv97@gmail.com>
2018-04-23 00:36:04 -0700

    Add option for teachers to create new courses

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get '/students', to: "students#show", as: 'student'
   resources :teachers
   resources :courses
+  authenticated :teacher do
+    root to: 'teachers#index', as: :authenticated_root
+  end
+
   root to: "home#index"
 
 end
