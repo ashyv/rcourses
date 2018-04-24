45a2a98b54b2e6d818178facadcb6b4b44e15b9b
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-04-24 02:11:43 -0700

    comment out seed to test adding student to course

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 [['Jon Snow', 26299532, 'jsnow@gmail.com'],['Daniel Caesar', 25644521, 'dcaesar@hotmail.com'], ['Gordon Ramsay', 20194827, 'lambsauce@gmail.com']].each do |name, sid, email|
   Student.create!(
     name: name,
-    student_id: sid,
+    cal_id: sid,
     email: email
   )
 end
@@ -16,21 +16,21 @@ end
 
 
 
-[['Jhene Aiko', 'jaiko@berkeley.edu'],['Frank Ocean', 'focean@berkeley.edu'], ['Donald Glover', 'childishgambino@berkeley.edu']].each do |name, email|
-  Teacher.create!(
-    name: name,
-    email: email
-  )
-end
+# [['Jhene Aiko', 'jaiko@berkeley.edu'],['Frank Ocean', 'focean@berkeley.edu'], ['Donald Glover', 'childishgambino@berkeley.edu']].each do |name, email|
+#   Teacher.create!(
+#     name: name,
+#     email: email
+#   )
+# end
 
 #how to create course?
 
-[['English 1', 1], ['Math 1', 2], ['Chemistry 1', 3]].each do |name, tid|
-  Course.create!(
-    name: name,
-    teacher_id: tid
-  )
-end
+# [['English 1', 1], ['Math 1', 2], ['Chemistry 1', 3]].each do |name, tid|
+#   Course.create!(
+#     name: name,
+#     teacher_id: tid
+#   )
+# end
 
 # [[26299532, 1], [25644521, 2], [20194827, 3]].each do |sid, cid|
 #   StudentCourse.create!(
@@ -38,5 +38,3 @@ end
 #   course_id: cid
 #   )
 # end
-Jon = Student.find(26299532)
-English1 = Course.find(1)
